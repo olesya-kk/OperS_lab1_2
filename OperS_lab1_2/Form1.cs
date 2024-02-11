17400e92ec2c6679a1f5162c42f898501d6f3315
Admin <Admin@LAPTOP-Q6I9MM59>
2024-02-11 19:24:46 +0300

    Добавьте файлы проекта.

---
+++ OperS_lab1_2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OperS_lab1_2
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            BackColor = SystemColors.Control;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Color randomColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+
+            BackColor = randomColor;
+        }
+    }
+}
